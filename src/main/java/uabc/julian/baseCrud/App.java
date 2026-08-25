764df1f28314c9743ee8350a54d3cc95931079e2
JulianAM5 <julian760x@outlook.com>
2026-08-24 21:04:57 -0700

    Estilos

--- src/main/java/uabc/julian/baseCrud/App.java
+++ src/main/java/uabc/julian/baseCrud/App.java
@@ -3,7 +3,9 @@ package uabc.julian.baseCrud;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import uabc.julian.baseCrud.view.PersonaFullView;
 
 /**
  * Hello world!
@@ -11,7 +13,13 @@ import javafx.stage.Stage;
 public class App extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new Pane(), 600, 800);
+        StackPane root = new StackPane();
+        Controlador controlador = new Controlador();
+
+        root.getChildren().add(new PersonaFullView(controlador, 1, ""));
+
+        Scene scene = new Scene(root, 600, 800);
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
 	}
