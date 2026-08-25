0ca201a13cc78fb9240d1209a90bf157c447be64
JulianAM5 <julian760x@outlook.com>
2026-08-24 19:47:06 -0700

    Controlador

---
+++ src/main/java/uabc/julian/baseCrud/view/Controller.java
@@ -0,0 +1,21 @@
+package uabc.julian.baseCrud.view;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Controller
+ */
+public class Controller {
+
+    public Controller() {
+
+    }
+
+    public void openPane(Pane pane) {
+
+    }
+
+    public void closePane(Pane pane) {
+
+    }
+}
