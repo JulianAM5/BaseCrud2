c1298022996428822c8ddf70dacfccba5e516d3f
JulianAM5 <julian760x@outlook.com>
2026-08-24 21:24:40 -0700

    Clase Persona

---
+++ src/main/java/uabc/julian/baseCrud/data/Persona.java
@@ -0,0 +1,23 @@
+package uabc.julian.baseCrud.data;
+
+/**
+ * Persona
+ */
+public class Persona {
+
+    private int id;
+    private String nombre;
+
+    public Persona(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
