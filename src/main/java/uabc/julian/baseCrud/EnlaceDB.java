ebaf21fb6d52625e4ebd513ac250ae3524a43db0
JulianAM5 <julian760x@outlook.com>
2026-08-24 23:50:06 -0700

    Recuperar Personas

---
+++ src/main/java/uabc/julian/baseCrud/EnlaceDB.java
@@ -0,0 +1,101 @@
+package uabc.julian.baseCrud;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import uabc.julian.baseCrud.data.Persona;
+
+/**
+ * EnlaceDB
+ */
+public class EnlaceDB {
+
+    // Datos de conexión a la base de datos
+    private static final String URL = "jdbc:mariadb://localhost:3306/agenda";
+    private static final String USER = "usuario1";
+    private static final String PASSWORD = "superpassword";
+
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    public EnlaceDB() {
+
+    }
+
+    public ArrayList<Persona> RecuperarPersonas() {
+        try {
+            ArrayList<Persona> personas = new ArrayList<>();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM Personas");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+
+                Persona persona = new Persona(id, nombre);
+
+                personas.add(persona);
+            }
+
+            return personas;
+        } catch (SQLException se) {
+            se.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    public Persona RecuperarPersona(int id) {
+        try {
+            String sqlInstruccion = "SELECT * FROM Personas where id = ?";
+            PreparedStatement ps = conn.prepareStatement(sqlInstruccion);
+            ps.setInt(1, id);
+            
+            rs = ps.executeQuery();
+
+            return new Persona(rs.getInt("id"), rs.getString("nombre"));
+        } catch (SQLException se) {
+            se.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    public void ConectarAServidor() {
+        try {
+            // 1. Registrar el driver JDBC
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            // 2. Establecer la conexión
+            System.out.println("Conectando a la base de datos...");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+    public void CerrarConexion() {
+        try {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+    }
+}
