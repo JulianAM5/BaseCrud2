66a1dc71a1205bcefcf0a1be37e2ed1b8466de1c
JulianAM5 <julian760x@outlook.com>
2026-08-24 22:13:59 -0700

    Mostrar direcciones y telefonos de persona

--- src/main/java/uabc/julian/baseCrud/view/PersonaFullView.java
+++ src/main/java/uabc/julian/baseCrud/view/PersonaFullView.java
@@ -1,5 +1,8 @@
 package uabc.julian.baseCrud.view;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -8,6 +11,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import uabc.julian.baseCrud.Controlador;
+import uabc.julian.baseCrud.data.Direccion;
+import uabc.julian.baseCrud.data.Telefono;
 
 /**
  * PersonaFullView
@@ -18,10 +23,10 @@ public class PersonaFullView extends VBox {
 
     public PersonaFullView(Controlador controlador, int id, String nombre) {
         this.controlador = controlador;
-        setLayout(nombre);
+        setLayout(nombre, id);
     }
 
-    private void setLayout(String nombre) {
+    private void setLayout(String nombre, int id) {
         Label nombreLabel = new Label(nombre);
         Button editarNombre = new Button("Editar");
 
@@ -33,9 +38,14 @@ public class PersonaFullView extends VBox {
         ScrollPane direccionesSPane = new ScrollPane(); 
         Button agregarDireccionButton = new Button("Agregar");
         
+        setTelefonos(telefonosSPane, id);
+        setDirecciones(direccionesSPane, id);
+
+        nombreLabel.getStyleClass().add("custom-title");
+        telefonosLabel.getStyleClass().add("custom-title");
+        direccionesLabel.getStyleClass().add("custom-title");
         agregarTelefonoButton.getStyleClass().add("custom-add-button");
         agregarDireccionButton.getStyleClass().add("custom-add-button");
-
         direccionesSPane.getStyleClass().add("custom-mini-scrollPane");
         telefonosSPane.getStyleClass().add("custom-mini-scrollPane");
 
@@ -45,5 +55,32 @@ public class PersonaFullView extends VBox {
         getChildren().addAll(nombreLabel, editarNombre, telefonosLabel, telefonosSPane, agregarTelefonoButton, direccionesLabel, direccionesSPane, agregarDireccionButton);
 
         setPadding(new Insets(10));
+    }
+
+    private void setTelefonos(ScrollPane pane, int personaId) {
+        ArrayList<Telefono> telefonos = controlador.solicitarTelefonosDePersona(personaId);
+        VBox holder = new VBox();
+
+        for (Telefono telefono : telefonos) {
+            holder.getChildren().add(new TelefonoSmallView(controlador, personaId, telefono.getNumeroTelefonico()));
+        }
+
+        holder.setSpacing(3);
+        pane.setFitToWidth(true);
+        pane.setContent(holder);
+    }
+
+    private void setDirecciones(ScrollPane pane, int personaId) {
+        ArrayList<Direccion> direcciones = controlador.solicitarDireccionesDePersona(personaId);
+
+        VBox holder = new VBox();
+
+        for (Direccion direccion : direcciones) {
+            holder.getChildren().add(new DireccionSmallView(controlador, personaId, direccion.getDireccion()));
+        }
+
+        holder.setSpacing(3);
+        pane.setFitToWidth(true);
+        pane.setContent(holder);
     }
 }
