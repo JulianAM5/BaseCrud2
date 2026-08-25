846e83fbf0fa5fb985f7ca5c3b1a4b8944df645f
JulianAM5 <julian760x@outlook.com>
2026-08-25 00:57:09 -0700

    Planificacion de Querys desde controlador

--- src/main/java/uabc/julian/baseCrud/Controlador.java
+++ src/main/java/uabc/julian/baseCrud/Controlador.java
@@ -1,19 +1,58 @@
 package uabc.julian.baseCrud;
 
+import java.util.ArrayList;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import uabc.julian.baseCrud.data.Direccion;
+import uabc.julian.baseCrud.data.Persona;
+import uabc.julian.baseCrud.data.Telefono;
+
 /**
  * Controlador
  */
 public class Controlador {
 
-    public Controlador() {
+    private StackPane root;
 
+    public Controlador(StackPane root) {
+        this.root = root;
     }
 
-    public void abrirPanel() {
-
+    public void abrirPanel(Pane pane) {
+        root.getChildren().add(pane);
     }
 
-    public void cerrarPanel() {
+    public void cerrarPanel(Pane pane) {
+        root.getChildren().remove(pane);
+    }
 
+    public ArrayList<Persona> solicitarPersonas() {
+        ArrayList<Persona> personas = new ArrayList<>();
+        return personas;
+    }
+
+    public ArrayList<Direccion> solicitarDirecciones() {
+        ArrayList<Direccion> direcciones = new ArrayList<>();
+        return direcciones;
+    }
+
+    public ArrayList<Direccion> solicitarDireccionesDePersona(int personaId) {
+        ArrayList<Direccion> direcciones = new ArrayList<>();
+        return direcciones;
+    }
+
+    public ArrayList<Telefono> solicitarTelefonosDePersona(int personaId) {
+        ArrayList<Telefono> telefonos = new ArrayList<>();
+        return telefonos;
+    }
+
+    public ArrayList<Persona> solicitarPersonasAsociadasADireccion(int direccionId) {
+        ArrayList<Persona> personas = new ArrayList<>();
+        return personas;
+    }
+
+    public boolean soliciarEdicionDeNombre() {
+        return true;
     }
 }
