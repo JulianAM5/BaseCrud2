83211b04694ab7123d5f0cda6ee7e9c1cb05d4e6
JulianAM5 <julian760x@outlook.com>
2026-08-29 00:47:47 -0700

    Iniciar y cerrar conexion MariaDB

--- src/main/java/uabc/julian/baseCrud/BaseDatos/MariaRepo.java
+++ src/main/java/uabc/julian/baseCrud/BaseDatos/MariaRepo.java
@@ -1,5 +1,8 @@
 package uabc.julian.baseCrud.BaseDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 import uabc.julian.baseCrud.data.Direccion;
@@ -10,6 +13,15 @@ import uabc.julian.baseCrud.data.Telefono;
  * MariaRepo
  */
 public class MariaRepo implements Repositorio {
+
+    private static final String URL = "jdbc:mariadb://localhost:3306/agenda_2";
+    private String USUARIO;
+    private String CONTRASEÑA;
+    private Connection connection;
+
+    public MariaRepo(String usuario, String contraseña) {
+        
+    }
 
 	@Override
 	public List<Persona> consultarTodasPersonas() {
@@ -141,5 +153,35 @@ public class MariaRepo implements Repositorio {
 	public boolean eliminarTelefono(int id) {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'eliminarTelefono'");
+	}
+
+	@Override
+    public boolean iniciarConexion() {
+        try {
+            // 1. Registrar el driver JDBC
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            // 2. Establecer la conexión
+            System.out.println("Conectando a la base de datos...");
+            connection = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+            return true;
+        } catch (SQLException se) {
+            se.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+	}
+
+	@Override
+	public boolean cerrarConexion() {
+        try {
+            if (connection != null) { connection.close(); }
+            return true;
+        } catch (SQLException se) {
+            se.printStackTrace();
+            return false;
+        }
 	}
 }
