5f84e283b972a99bf723385148861a8f318abd40
JulianAM5 <julian760x@outlook.com>
2026-08-30 22:29:17 -0700

    PersonasView Panel

---
+++ src/main/java/uabc/julian/baseCrud/view/PersonasView.java
@@ -0,0 +1,61 @@
+package uabc.julian.baseCrud.view;
+
+import java.util.ArrayList;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import uabc.julian.baseCrud.controladores.Lector;
+import uabc.julian.baseCrud.data.Persona;
+
+/**
+ * PersonasView
+ */
+public class PersonasView extends VBox {
+
+    private Lector lector;
+    private Creador creador;
+
+    public PersonasView(Lector lector) {
+        this.lector = lector;
+        setLayout();
+    }
+
+    private void setLayout() {
+        getChildren().clear();
+        Label tituloLabel = new Label("REGISTRO");
+
+        ScrollPane scrollPane = new ScrollPane();
+
+        Button botonAgregar = new Button("Agregar");
+
+        tituloLabel.getStyleClass().add("custom-title");
+        scrollPane.getStyleClass().add("custom-full-scrollPane");
+        botonAgregar.getStyleClass().add("custom-add-button");
+
+        cargarPersonas(scrollPane, botonAgregar);
+        setPadding(new Insets(10));
+
+        setAlignment(Pos.CENTER);
+        getChildren().addAll(tituloLabel, scrollPane, botonAgregar);
+    }
+
+    private void cargarPersonas(ScrollPane scrollPane, Button botonAgregar) {
+        ArrayList<Persona> personas = new ArrayList<>(lector.leerPersonas());
+        VBox holder = new VBox();
+
+        botonAgregar.setOnAction(e -> {
+        });
+
+        for (Persona persona : personas) {
+            holder.getChildren().add(creador.crearSmallView(persona));
+        }
+
+        holder.setSpacing(3);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setContent(holder);
+    }
+}
