061a81eb780cdfa85a3c1f1df8d0857787489c92
JulianAM5 <julian760x@outlook.com>
2026-08-30 22:16:19 -0700

    Interface Creador

---
+++ src/main/java/uabc/julian/baseCrud/view/Creador.java
@@ -0,0 +1,16 @@
+package uabc.julian.baseCrud.view;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Creador
+ */
+public interface Creador {
+    Pane crearModificarPanel();
+    Pane crearFullViewPersonaPanel();
+    Pane crearFullViewDireccionesPanel();
+    Pane crearPreviewPersonasPanel();
+    Pane crearPreviewDireccionesPanel();
+    Pane crearAgregarPersonasPanel();
+    Pane crearAgregarDireccionesPanel();
+}
